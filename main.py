4ddd0f027ab50bf68717cb16a190d66c5ec07225
Vishal Dhara <182242477+vishaldhara31@users.noreply.github.com>
2026-01-21 23:42:30 +0530

    Fix FastAPI initialization and update endpoint functions
    Refactor FastAPI application with updated function names and comments.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FASTAPI
+from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
-app = FASTAPI()
+app = FastAPI()
+
 
 class Post(BaseModel):
     title : str
@@ -11,34 +13,26 @@ class Post(BaseModel):
     published : bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title" : "title of the first post", "content":"Content of the first post", "id": 1}  
+            ,{"title": "Library", "content":"This library content all types of book", "id" : 2}]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/post') 
-#here the @ is used as we initialise the decorator and we use different https method and in the write the different urls like here example /post
-async def post(): #this is just a simple function
-     return{"message" : "This is the request send to post"} #return a dictionary
+@app.get("/") #decorator
+async def root(): #this is just a simple function and async for long duration task
+    return {"message": "My world of api"}
 
 
-# #the below is the post method , and we can test this url from different software, i am using the postman
-# @app.post("/createpost")
-# def create_posts(payload: dict = Body(...)): #what this do is take the content from the body and convert that content into the dictionary and store in payload
-#     print(payload)
-#     return{"message": "This post created successfully"}
+@app.get("/posts")
+def get_posts():
+    return{"data" : my_posts}
 
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0,1000000)
+    my_posts.append(post_dict)
+    return{"data" : post_dict} 
 
-# the above is the same function which is commented but this is directly checking the schemas using the pydantic module and checking the data properly
-@app.post("/createpost")
-def create_posts(new_post:Post): #what this do is take the content from the body and convert that content into the dictionary and store in payload
-    print(new_post)
-    print(new_post.title)
-    print(new_post.content)
-    print(new_post.published)
-    return{"data": "new_posts"}
-
-
-
-
+@app.get("/posts/{id}")
+def get_post(id):
+    print(id)
+    return {"Post_Details" : f"Your Post Details {id}"}
