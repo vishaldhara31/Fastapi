595865a483f62ddd92e994285d5a40382cf77ac8
Vishal Dhara <182242477+vishaldhara31@users.noreply.github.com>
2026-01-30 08:27:03 +0530

    Refactor FastAPI to use PostgreSQL for CRUD operations
    Refactor the FastAPI application to use PostgreSQL for CRUD operations instead of in-memory storage. Update database connection details and modify endpoints for creating, retrieving, updating, and deleting posts.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+
 app = FastAPI()
 
 
@@ -13,15 +14,12 @@ class Post(BaseModel):
     title : str
     content : str
     published : bool = True
-    rating: Optional[int] = None
-
+    # rating: Optional[int] = None
 
 while True:
     try:
-
-        conn = psycopg2.connect(host = ' ', database = ' ', user = ' '
-                                , password = ' ', cursor_factory= RealDictCursor)   
-
+        conn = psycopg2.connect(host = 'localhost', database = 'fastapi', user = 'postgres'
+                                , password = 'god', cursor_factory= RealDictCursor)   
         cursor = conn.cursor()
         print("Database connection is successfull!") 
         break
@@ -44,69 +42,57 @@ def find_post(id):
     for p in my_posts:
         if p["id"] == id:
          return p
-        
-
+            
 def find_index_post(id):
     for i, p in enumerate(my_posts):
         if p['id'] == id:
             return i    
 
-
 #getting all the post
 @app.get("/posts")
 def get_posts():
-    return{"data" : my_posts}
-
-
+    cursor.execute(""" SELECT * FROM posts """)
+    posts = cursor.fetchall()
+    return{"data" : posts}
 
 #creating the post
 #here we change the http response using status code
 @app.post("/posts", status_code = status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.model_dump()
-    post_dict['id'] = randrange(0,1000000)
-    my_posts.append(post_dict)
-    return{"data" : post_dict} 
-#this is a dictonary and it convertes to a json file later
+    cursor.execute(""" INSERT INTO posts(title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+                   (post.title, post.content, post.published))
+    new_posts = cursor.fetchone()
+    conn.commit()
+    return{"data" : new_posts} #this is a dictonary and it convertes to a json file later
 
-
-#getting one individual post
+# getting one individual post
 @app.get("/posts/{id}") #here the {id} is the path parameter
 def get_post(id : int):
-    post = find_post(id)#the path function return the id in string so to work we should convert that parameter in int
+    cursor.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id),))
+    post = cursor.fetchone()
+    # post = find_post(id)#the path function return the id in string so to work we should convert that parameter in int
     if not post:
-        raise HTTPException (status_code=status.HTTP_404_NOT_FOUND,detail = f"post with{id}: was not found")
+        raise HTTPException (status_code= status.HTTP_404_NOT_FOUND,detail = f"post with {id} was not found")
     return {"Post_Details" :  post}
 
-#deleting a post
+#delete
 @app.delete("/posts/{id}", status_code = status.HTTP_204_NO_CONTENT)
 def delete_post(id : int):
-    index = find_index_post(id)
-
-    if index == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail= f"post with{id} does not exist")
-
-
-    my_posts.pop(index)
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    if deleted_post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail= f"post with id: {id} does not exist")
     return Response(status_code=status.HTTP_204_NO_CONTENT)
-# We return 204 No Content for DELETE requests
-# to indicate successful deletion without sending any response data.
 
-
-#updating 
+#update
 @app.put("/posts/{id}")
-def get_post(id: int, post:Post):
-
-    
-    index = find_index_post(id)
-
-    if index == None:
+def update_post(id: int, post:Post):
+    cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, 
+                   (post.title, post.content, post.published, str(id),))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if updated_post == None:
        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND ,
-                            detail= f"post with{id} does not exist")
-    
-    post_dict = post.model_dump() #here the data we get from the post which is written in the function and converted into regular python dictionary
-    post_dict["id"] = id  #this line add the id in each post dictionary , this id does not send by the user it is copy from the path url 
-    my_posts[index] = post_dict #this line replace the context in the particular index of my_post. It get replaced with the content of present in post_dict
-
-    return{"data" : post_dict}
-
+                            detail= f"post with {id} does not exist")
+    return{"data" : updated_post}
