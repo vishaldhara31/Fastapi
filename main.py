46a06b4a62795202313bc5040e071dd62717b85d
Vishal Dhara <182242477+vishaldhara31@users.noreply.github.com>
2026-01-31 10:21:40 +0530

    Set up database and session handling
    Initialize database and create session management.

--- main.py
+++ main.py
@@ -6,9 +6,19 @@ from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from . import models
+from .database import engine,SessionLocal
+
+models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 class Post(BaseModel):
     title : str
