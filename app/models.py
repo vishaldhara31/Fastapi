91484038b39bb0a1cca817fee3c6141c37a9a931
Vishal Dhara <182242477+vishaldhara31@users.noreply.github.com>
2026-02-01 14:37:40 +0530

    Enhance Post model with created_at field
    Added created_at timestamp field to Post model.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 #Every models represented a table in our database
+#sqlalchemy doesnt ment to make migration(making changes in the table) in our table we have to use different software
 from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 from .database import Base
 
 class Post(Base):
@@ -9,4 +11,5 @@ class Post(Base):
     id = Column(Integer, primary_key = True, nullable= False)
     title = Column(String, nullable= False)
     content = Column(String, nullable= False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, nullable= False, server_default= 'TRUE')
+    created_at = Column(TIMESTAMP(timezone=True), nullable = False, server_default= text('now()'))
